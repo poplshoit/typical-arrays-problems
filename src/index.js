88c5ae186ad0e65cabdaf2c10c0d80ff3f2ae850
Maksim Popushoi <poplshoy@gmail.com>
2020-10-01 12:03:39 +0300

    feat: Impliment task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(!array || !array.length) {
+    return 0;
+  };
+  let minCount = array[0];
+  for(var i = 0; i < array.length; i++) {
+    if(minCount > array[i]) {
+     minCount = array[i];
+    }
+  }
+  return minCount;
+} 
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(!array || !array.length) {
+    return 0;
+  };
+  let maxCount = array[0];
+  for(var i = 0; i < array.length; i++) {
+    if(maxCount < array[i]) {
+     maxCount = array[i];
+    }
+  }
+  return maxCount;
+} 
+
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length) {
+    return 0;
+  };
+  let sumArray = 0;
+    for(let i = 0; i < array.length; i++) {
+      sumArray += array[i];
+    }
+    return sumArray / array.length ;
 }
